629f933d6461b04c4f4756f6849d614f3e41752f
kenji oyamada <oyaken0717@yahoo.co.jp>
2019-01-24 08:34:43 +0000

    削除機能完了

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,5 @@
 class BlogsController < ApplicationController
+    before_action :set_blog, only: [:show, :edit, :update, :destroy]
     def index
         @blogs = Blog.all
     end
@@ -11,7 +12,7 @@ class BlogsController < ApplicationController
         @blog = Blog.new(blog_params)
         if @blog.save
           # 一覧画面へ遷移して"ブログを作成しました！"とメッセージを表示します。
-          redirect_to blogs_path, notice: "ブログを作成しました！"
+          redirect_to blogs_path, notice: "投稿しました！"
         else
           # 入力フォームを再描画します。
           render 'new'
@@ -29,10 +30,15 @@ class BlogsController < ApplicationController
     def update
         @blog = Blog.find(params[:id])
         if @blog.update(blog_params)
-          redirect_to blogs_path, notice: "ブログを編集しました！"
+          redirect_to blogs_path, notice: "編集しました！"
         else
           render 'edit'
         end
+    end
+    
+    def destroy
+        @blog.destroy
+        redirect_to blogs_path, notice:"削除しました！"
     end
 
       private
@@ -40,4 +46,9 @@ class BlogsController < ApplicationController
       def blog_params
         params.require(:blog).permit(:title)
       end
+      
+      def set_blog
+        @blog = Blog.find(params[:id])
+      end
+      
 end
