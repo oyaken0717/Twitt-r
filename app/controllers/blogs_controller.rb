364f120a417e281cb617d62be61268edbf7480aa
kenji oyamada <oyaken0717@yahoo.co.jp>
2019-01-24 02:11:56 +0000

    一覧表示完了

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
     def index
+        @blogs = Blog.all
     end
 
     def new
@@ -14,6 +15,6 @@ class BlogsController < ApplicationController
       private
     
       def blog_params
-        params.require(:blog).permit(:title, :content)
+        params.require(:blog).permit(:title)
       end
 end
