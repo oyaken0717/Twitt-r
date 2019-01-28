06be9e28bbe1e7ea8ada0925523e18220380b195
kenji oyamada <oyaken0717@yahoo.co.jp>
2019-01-28 03:58:38 +0000

    topページ再作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'tops/index'
+
   resources :tweets do
     collection do
       post :confirm
     end
-    
-  
   end
+  
+  root to: 'tops#index'
   
 end
